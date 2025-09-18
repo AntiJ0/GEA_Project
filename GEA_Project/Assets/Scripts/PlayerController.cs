94d85af8a2ab534de5526a54406c6bcf0e321879
AntiJ0 <new060717gmail.com>
2025-09-18 15:28:58 +0900

    [Feat] 캐릭터 달리기 및 카메라 변환 추가

--- GEA_Project/Assets/Scripts/PlayerController.cs
+++ GEA_Project/Assets/Scripts/PlayerController.cs
@@ -1,36 +1,92 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+    public float sprintSpeed = 10f;  // 달리기 속도
     public float jumpPower = 5f;
     public float gravity = -9.81f;
+
+    public CinemachineVirtualCamera virtualCam;
+    public float rotationSpeed = 10f;
+    private CinemachinePOV pov;
     private CharacterController controller;
     private Vector3 velocity;
     public bool isGrounded;
 
+    // 달리기 체크용 변수
+    private bool isSprinting;
+
+    // FreeLook 카메라 줌 조정
+    public CinemachineFreeLook freeLookCam;
+    public float zoomOutFOV = 60f; // 달릴 때 줌 아웃되는 FOV 값
+    private float defaultFOV;
+
+    public CinemachineSwitcher camSwitcher;
+
+    // CinemachineSwitcher 스크립트와 연결할 변수
+    public bool usingFreeLook = false;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        pov = virtualCam.GetCinemachineComponent<CinemachinePOV>();
+
+        // FreeLook 카메라의 기본 FOV 저장
+        defaultFOV = freeLookCam.m_Lens.FieldOfView;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        isGrounded = controller.isGrounded;
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        if (camSwitcher != null)
+            usingFreeLook = camSwitcher.usingFreeLook;
 
-        Vector3 move = new Vector3(x, 0, z);
-        controller.Move(move * speed * Time.deltaTime);
+        if (usingFreeLook)
+            return;
+
+        isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        isSprinting = Input.GetKey(KeyCode.LeftShift);
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 camForward = virtualCam.transform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+
+        Vector3 camRight = virtualCam.transform.right;
+        camRight.y = 0;
+        camRight.Normalize();
+
+        Vector3 move = (camForward * z + camRight * x).normalized;
+        float currentSpeed = isSprinting ? sprintSpeed : speed;
+        Vector3 moveDirection = move * currentSpeed;
 
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             velocity.y = jumpPower;
         }
+
         velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+
+        Vector3 finalMove = moveDirection;
+        finalMove.y = velocity.y;
+
+        controller.Move(finalMove * Time.deltaTime);
+
+        float cameraYaw = pov.m_HorizontalAxis.Value;
+        Quaternion targetRot = Quaternion.Euler(0f, cameraYaw, 0f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotationSpeed * Time.deltaTime);
+
+        freeLookCam.m_Lens.FieldOfView = isSprinting ? zoomOutFOV : defaultFOV;
     }
 }
